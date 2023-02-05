6dddbc378b294fe7573aadf2c3181ec41c036500
Mishganchek <122285943+Mishganchek@users.noreply.github.com>
2023-02-05 15:16:06 +0300

    Game1

--- thief/Assets/Scripts/Alarm.cs
+++ thief/Assets/Scripts/Alarm.cs
@@ -5,7 +5,7 @@ public class Alarm : MonoBehaviour
     [SerializeField] private AudioSource _alarm;
     private float _speed = 0.05f;
     private float _maxVolume = 1.0f;
-    private float _startVolume = 0.01f;
+    private float _startVolume = 0f;
     private float _currentVolume = 0.01f;
     private bool _IsOut;
 
@@ -20,7 +20,7 @@ public class Alarm : MonoBehaviour
         _speed += 0.1f;
 
         Debug.Log(_alarm.volume);
-        _currentVolume = _maxVolume;
+        _currentVolume = _alarm.volume;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,9 +38,9 @@ public class Alarm : MonoBehaviour
     {
         if (_IsOut)
         {
-        _alarm.volume = Mathf.MoveTowards(_currentVolume, _startVolume, _speed * Time.deltaTime);
-        _speed += 0.1f;
-
+            Debug.Log(_alarm.volume);
+            _alarm.volume = Mathf.MoveTowards(_currentVolume, _startVolume, _speed * Time.deltaTime);
+            _speed += 0.1f;
         }
     }
 }
