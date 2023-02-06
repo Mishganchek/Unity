a988ca81511db62e12339ba66fd2e751db3b80a7
Mishganchek <122285943+Mishganchek@users.noreply.github.com>
2023-02-06 20:23:05 +0300

    Thief

--- thief/Assets/Scripts/Alarm.cs
+++ thief/Assets/Scripts/Alarm.cs
@@ -1,46 +1,59 @@
+using System.Collections;
 using UnityEngine;
 
 public class Alarm : MonoBehaviour
 {
     [SerializeField] private AudioSource _alarm;
-    private float _speed = 0.05f;
+    [SerializeField] private float _speed;
     private float _maxVolume = 1.0f;
-    private float _startVolume = 0f;
-    private float _currentVolume = 0.01f;
-    private bool _IsOut;
+    private Coroutine _coroutine;
+
 
     private void Start()
     {
         _alarm.volume = 0f;
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        _alarm.volume = Mathf.MoveTowards(_startVolume, _maxVolume, _speed * Time.deltaTime);
-        _speed += 0.1f;
-
-        Debug.Log(_alarm.volume);
-        _currentVolume = _alarm.volume;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         _alarm.Play();
-        _alarm.volume = 0f;
+
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+
+        _coroutine = StartCoroutine(IncreaseVolum());
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _IsOut = true;
+        if (_coroutine != null)
+        {
+
+            StopCoroutine(_coroutine);
+        }
+
+        StopCoroutine(_coroutine);
+        _coroutine = StartCoroutine(DecreaseVolum());
     }
 
-    private void Update()
+    private IEnumerator IncreaseVolum()
     {
-        if (_IsOut)
+        while (_alarm.volume < 1)
         {
-            Debug.Log(_alarm.volume);
-            _alarm.volume = Mathf.MoveTowards(_currentVolume, _startVolume, _speed * Time.deltaTime);
-            _speed += 0.1f;
+
+            _alarm.volume = Mathf.MoveTowards(_alarm.volume, _maxVolume, _speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator DecreaseVolum()
+    {
+        while (_alarm.volume > 0)
+        {
+            _alarm.volume = Mathf.MoveTowards(_alarm.volume, 0, _speed * Time.deltaTime);
+            yield return null;
         }
     }
 }
